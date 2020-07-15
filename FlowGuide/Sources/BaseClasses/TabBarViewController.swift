6a5dfd319a91fbd18c7f47264b64266e8c3a1159
Vinod <>
2020-07-15 12:18:28 +0530

    Force unwrap of TabBar font changed to Default

--- FlowGuide/Sources/BaseClasses/TabBarViewController.swift
+++ FlowGuide/Sources/BaseClasses/TabBarViewController.swift
@@ -17,7 +17,7 @@ class TabBarViewController: UITabBarController {
        configureUI()
     }
     func configureUI() {
-        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: FontType.avenirNextCondensed.rawValue, size: 18) as Any], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: FontType.avenirNextCondensed.rawValue, size: 18) ?? UIFont.systemFont(ofSize: 15.0, weight: UIFont.Weight.light)], for: .normal)
         UITabBar.appearance().unselectedItemTintColor = UIColor.lightText
     }
     /*
